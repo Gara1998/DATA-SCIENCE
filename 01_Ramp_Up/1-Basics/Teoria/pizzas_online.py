da28582b7ec75bea0b9374b061e1225a509ec594
Gara1998 <163641406+Gara1998@users.noreply.github.com>
2024-03-16 11:27:47 +0100

    Delete 01_Ramp_Up directory

--- 01_Ramp_Up/1-Basics/Teoria/pizzas_online.py
+++
@@ -1,9 +0,0 @@
-print("Buenas tardes, bienvenido al servicio de pedido online, ¿Cuántas pizzas desea?")
-
-pizz = input()
-# print(pizz)
-
-direcc = input(f"Estupendo,\n se están preparando {pizz} pizzas. Digame su dirección")
-# print(direcc)
-
-print(f"Le mandaremos las {pizz} pizzas a la dirección {direcc}. Muchas gracias por su pedido.")
--- 01_Ramp_Up/6-Modulos/Teoria/direc_segundo/segundo_script.py
+++
@@ -1,6 +0,0 @@
-
-c = 3
-
-d = 4
-
-string_var = "Probando"
